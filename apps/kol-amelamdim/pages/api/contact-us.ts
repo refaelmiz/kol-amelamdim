b47de562b1052b9643c85ec249bcc294aa02159c
Netanel Vaknin <emailofnetanel@gmail.com>
2023-01-24 22:46:39 +0200

    Integrate with schooliland

--- apps/kol-amelamdim/pages/api/contact-us.ts
+++ apps/kol-amelamdim/pages/api/contact-us.ts
@@ -2,12 +2,23 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { API_ERRORS } from '@kol-amelamdim/api-errors';
 import { Lead } from '@kol-amelamdim/models';
 import connect from '../../db/connectMongo';
+import api from "../../api";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { customerEmail, customerQuestion, from } = req.body;
+
+  try {
+    await api.post('https://courses.kol-amelamdim.co.il/wp-json/uap/v2/uap-30171-30172', {
+      customerEmail: customerEmail,
+      customerQuestion: customerQuestion,
+      from,
+    })
+  } catch (e) {
+    console.log(e);
+  }
 
   try {
     await connect();
